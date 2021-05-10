7248aa4ecdc201076f357296ec20b7861bfcf940
bogdanov <iceposhta@gmail.com>
2021-05-10 13:45:31 +0100

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by bogdanov on 10.05.21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

460ad649ec54557b62cfdd2eaa93721708d68f84
bogdanov <iceposhta@gmail.com>
2021-05-11 18:38:49 +0100

    create PostGridView

--- InstagramClone/View/Search/SearchView.swift
+++ InstagramClone/View/Search/SearchView.swift
@@ -15,7 +15,7 @@ struct SearchView: View {
             // search bar
             SearchBar(text: $searchText)
                 .padding()
-            // grid view/user list view
+            PostGridView()
         }
     }
 }

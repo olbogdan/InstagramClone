4267ed7d43c7058eab499e954f5e5af920663a3d
bogdanov <iceposhta@gmail.com>
2021-05-13 00:05:31 +0100

    implemented upload post page UI

--- InstagramClone/View/UploadPost/UploadPostView.swift
+++ InstagramClone/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+
     var body: some View {
-        Text("upload")
+        VStack {
+            if postImage == nil {
+                Button(action: /*@START_MENU_TOKEN@*/ {}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "icloud.and.arrow.up.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 180, height: 180)
+                        .padding(.top, 56)
+                        .foregroundColor(.black)
+                }
+            } else {
+                HStack(alignment: .top) {
+                    Image("img1")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96, height: 96)
+                        .clipped()
+
+                    TextField("Enter your caption..", text: $captionText)
+                }
+                .padding()
+
+                Button(action: /*@START_MENU_TOKEN@*/ {}/*@END_MENU_TOKEN@*/) {
+                    Text("Share")
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(width: 360, height: 50)
+                        .background(Color.blue)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }
+                .padding()
+            }
+            Spacer()
+        }
     }
 }
 

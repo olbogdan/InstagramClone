c45de8f59a82ab615ae661c673028742c6fa9d96
bogdanov <iceposhta@gmail.com>
2021-05-17 22:52:20 +0100

    setup firebase

--- InstagramClone/InstagramCloneApp.swift
+++ InstagramClone/InstagramCloneApp.swift
@@ -5,10 +5,15 @@
 //  Created by bogdanov on 10.05.21.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct InstagramCloneApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             MainTabView()

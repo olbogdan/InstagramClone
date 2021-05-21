ea8792dbb9ad6351a7a73167f631b300bb8d2272
bogdanov <iceposhta@gmail.com>
2021-05-21 12:51:55 +0100

    implemented: an individual navigation bar title for each TabView

--- InstagramClone/View/Main/ContentView.swift
+++ InstagramClone/View/Main/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @EnvironmentObject var viewModel: AuthViewModel
+    @State var selectedIndex = 0
 
     var body: some View {
         Group {
@@ -16,7 +17,7 @@ struct ContentView: View {
                 LoginView()
             } else {
                 if let user = viewModel.currentUser {
-                    MainTabView(user: user)
+                    MainTabView(user: user, selectedIndex: $selectedIndex)
                 } else {
                     ProgressView()
                 }

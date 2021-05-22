daa9e92483c059002753eb90094a74cfda3a3e07
bogdanov <iceposhta@gmail.com>
2021-05-22 23:32:25 +0100

    implemented: notification data model

---
+++ InstagramClone/Model/Notification.swift
@@ -0,0 +1,33 @@
+//
+//  Notification.swift
+//  InstagramClone
+//
+//  Created by bogdanov on 22.05.21.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Notification: Identifiable, Decodable {
+    @DocumentID var id: String?
+    var postId: String?
+    let userName: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    let type: Int
+    let uid: String
+}
+
+enum NotificationType: Int, Decodable {
+    case like
+    case comment
+    case follow
+
+    var notificationMessage: String {
+        switch self {
+            case .like: return " liked one of your posts."
+            case .comment: return " commented on one of your posts."
+            case .follow: return " started following you."
+        }
+    }
+}

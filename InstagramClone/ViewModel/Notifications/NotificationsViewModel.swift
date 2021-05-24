c762c2a8d3b17ef22f83876c8dee46d70d89ff85
bogdanov <iceposhta@gmail.com>
2021-05-24 15:47:32 +0100

    bugfix: don't send notifications for actions that done by current user

--- InstagramClone/ViewModel/Notifications/NotificationsViewModel.swift
+++ InstagramClone/ViewModel/Notifications/NotificationsViewModel.swift
@@ -31,6 +31,7 @@ class NotificationsViewModel: ObservableObject {
 
     static func uploadNotification(toUid uid: String, type: NotificationType, post: Post? = nil) {
         guard let user = AuthViewModel.shared.currentUser else { return }
+        guard uid != user.uid else { return }
 
         var data: [String: Any] = [
             "timestamp": Timestamp(date: Date()),
